4f4d2d9d722bb18180af91a3dfbc6aeafb11d71e
Gabriel Lacey <g.lacey48@yahoo.com>
2022-09-12 14:32:14 +0100

    [~] Moved asteroid controller onto one scene object instead of individual asteroids [+] Added asteroid field generation, tied to the player's location

--- Assets/AteroidController.cs
+++ Assets/AteroidController.cs
@@ -4,21 +4,82 @@ using UnityEngine;
 
 public class AteroidController : MonoBehaviour
 {
+    [SerializeField] private GameObject asteroidPrefab;
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private int numAsteroids;
+    [SerializeField] private float spawnRange;
+    [SerializeField] private Vector2 scaleRange;
     [SerializeField] private float rotationSpeed = 3.5f;
-    [SerializeField] private Vector3 rotationAxis;
-    private Rigidbody rb;
+
+    private List<GameObject> asteroidCollection = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        rotationAxis = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
-        rb = this.gameObject.GetComponent<Rigidbody>();
-        rb.angularDrag = 0.0f;
-        rb.AddTorque(rotationAxis * rotationSpeed, ForceMode.Impulse);
+
+        float astRadius = asteroidPrefab.GetComponent<BoxCollider>().bounds.extents.x * 2;
+
+        for (int i = 0; i < numAsteroids; i++)
+        {
+            GameObject asteroidInst = Instantiate(asteroidPrefab, Vector3.zero, Quaternion.identity);
+            asteroidInst.SetActive(false);
+            asteroidCollection.Add(asteroidInst);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Prune existing asteroids
+        foreach (var asteroid in asteroidCollection)
+        {
+            if (Vector3.Distance(asteroid.transform.position, playerObject.transform.position) > spawnRange)
+            {
+                asteroid.SetActive(false);
+            }
+        }
+
+        // Spawn new asteroids
+        SpawnAsteroid();
     }
+
+    void SpawnAsteroid()
+    {
+        GameObject asteroidInst = new GameObject();
+        bool found = false;
+        // Find inactive asteroid
+        foreach (var asteroid in asteroidCollection)
+        {
+            if (asteroid.activeInHierarchy == false)
+            {
+                asteroidInst = asteroid;
+                found = true;
+            }
+        }
+
+        if (found)
+        {
+            Vector3 spawnPosition = Random.insideUnitSphere;
+            spawnPosition *= spawnRange;
+            spawnPosition += playerObject.transform.position;
+            spawnPosition.y = -10.0f;
+            asteroidInst.transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
+            float astScale = Random.Range(scaleRange.x, scaleRange.y);
+            float instRad = 2.0f * astScale;
+            Rigidbody rb = asteroidInst.GetComponent<Rigidbody>();
+            Vector3 rotationAxis = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            rotationAxis.Normalize();
+            rb.angularDrag = 0.0f;
+            asteroidInst.transform.localScale = new Vector3(astScale, astScale, astScale);
+            if (Physics.CheckSphere(spawnPosition, instRad) == false)
+            {
+                asteroidInst.SetActive(true);
+                rotationAxis *= Random.Range(0.01f, 0.2f) * astScale;
+                Debug.Log("Torque: " + rotationAxis);
+                rb.AddRelativeTorque(rotationAxis, ForceMode.Impulse);
+            }
+        }
+
+    }
+
 }
